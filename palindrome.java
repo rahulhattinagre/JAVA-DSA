d3ad7dd501e76fb2b25faffd4e9cfd8748b168c3
rahulhattinagre <rahulhattinagre@gmail.com>
2026-02-01 11:55:42 +0530

    message

--- palindrome.java
+++ palindrome.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
 
 public class palindrome {
-    public static boolean palindromenum (int n){
-        int palindrome=n;
-        if(isPalindrome(n)){
-            return true;
+    public static boolean isPalindrome(String str) {
+        // Remove non-alphanumeric characters and convert to lowercase
+        String cleaned = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int left = 0;
+        int right = cleaned.length() - 1;
+        while (left < right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
         }
-        return false;
+        return true;
     }
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int n=sc.nextInt();
 
-        System.out.println(n);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string to check if it's a palindrome: ");
+        String input = sc.nextLine();
+        System.out.println("Is palindrome: " + isPalindrome(input));
     }
-    
 }
