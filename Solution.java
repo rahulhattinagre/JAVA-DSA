a22e46938d48d9305dbbd9fc44c3fb3e02607817
Hattinagre Rahul Govind <157114025+rahulhattinagre@users.noreply.github.com>
2026-02-03 09:39:55 +0530

    Fix binary search logic in searchInsert method

--- Solution.java
+++ Solution.java
@@ -5,7 +5,16 @@ class Solution {
         int start=0, end=nums.length-1;
         while(start<=end){
             int mid=(start+ end)/2;
-
+     if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[mid]<target){
+                //right side
+                start=mid+1;
+            }else{//left
+                end=mid-1;
+            }
+        } 
            
         return start;
     }
