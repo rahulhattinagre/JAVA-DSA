aa101e826c6cd510cac9f4bc3862f2f3ad979650
Hattinagre Rahul Govind <157114025+rahulhattinagre@users.noreply.github.com>
2026-02-03 09:39:31 +0530

    Remove binary search implementation
    Removed binary search logic for finding target index.

--- Solution.java
+++ Solution.java
@@ -6,16 +6,7 @@ class Solution {
         while(start<=end){
             int mid=(start+ end)/2;
 
-            if(nums[mid]==target){
-                return mid;
-            }
-            if(nums[mid]<target){
-                //right side
-                start=mid+1;
-            }else{//left
-                end=mid-1;
-            }
-        } 
+           
         return start;
     }
     public static void main(String[]args){
