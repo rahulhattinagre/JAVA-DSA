2052f49af0ba1254c940293460f828995ac20e57
Hattinagre Rahul Govind <157114025+rahulhattinagre@users.noreply.github.com>
2026-02-09 10:07:55 +0530

    Clean up main method by removing unused code
    Removed commented-out code for input and search functionality.

--- TwoDArray.java
+++ TwoDArray.java
@@ -2,30 +2,7 @@ import java.util.Scanner;
 import java.util.*;
 
 public class TwoDArray {
-    /*public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int row=sc.nextInt();
-        int col=sc.nextInt();
-        int[][] num=new int[row][col];
-        //input 
-        //rowss
-        for(int i=0;i<row;i++){
-            //cols
-            for(int j=0;j<col;j++){
-                num[i][j]= sc.nextInt();
-            }
-        }
-        System.out.print("Enter a number for search : ");
-        int x=sc.nextInt();
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(num[i][j]==x){
-                    System.out.println("num is found :"+i+ ","+j);
-         }
-        }
-    }*/
-
-       // Print the spiral order matrix as output for a given matrix of numbers. 
+ matrix as output for a given matrix of numbers. 
        public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int row=sc.nextInt();
