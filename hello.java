14bc00a404f6f843953bcff61be0471f11ea729f
Hattinagre Rahul Govind <157114025+rahulhattinagre@users.noreply.github.com>
2026-02-11 11:29:51 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,13 +2,6 @@ import java.util.*;
 import java.util.Arrays;
 public class hello{
     public static void main(String[] args) {
-        int[] marks= new int[3];
-        marks[0]=82;
-        marks[1]=77;
-        marks[2]=91;
-
-        System.out.println(marks.length);
-        System.out.println("hello rahul");
-        
+    
     }
 }
