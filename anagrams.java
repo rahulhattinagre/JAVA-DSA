a22f025faf1576426b6911dc2141ed6c95d9bb7d
rahulhattinagre <rahulhattinagre@gmail.com>
2026-02-15 22:53:20 +0530

    message

---
+++ anagrams.java
@@ -0,0 +1,29 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class anagrams {
+    public static void main(String[] args) {
+        String str1="race";
+        String str2="care";
+        //convert to lowercase
+        str1=str1.toLowerCase();
+        str2=str2.toLowerCase();
+
+        if(str1.length()==str2.length()){
+            char[] str1charArray=str1.toCharArray();
+            char[] str2charArray=str2.toCharArray();
+            
+            Arrays.sort(str1charArray);
+            Arrays.sort(str2charArray);
+
+            boolean result=Arrays.equals(str1charArray,str2charArray);
+            if(result){
+                System.out.println(str1+" and "+str2+" are anagrams character.");
+            }else{
+                System.out.println(str1+" and "+str2+" are not anagrams character. ");
+            }
+        }else{
+            System.out.println(str1+" and "+str2+" length is not equal.");
+        }
+        }
+    }
