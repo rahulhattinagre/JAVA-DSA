71a2b124e7fb6651ec8772956da3cc3db2aca216
Hattinagre Rahul Govind <157114025+rahulhattinagre@users.noreply.github.com>
2026-02-17 22:38:43 +0530

    Update butterflyPatt.java

--- butterflyPatt.java
+++ butterflyPatt.java
@@ -15,17 +15,6 @@ public class butterflyPatt {
             }
             System.out.println();
         }
-        for(int i=n;i>=1;i--){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            for(int j=1;j<=n-i;j++){
-                System.out.print( "  ");
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println( );
-    }
+       
 }
 }
