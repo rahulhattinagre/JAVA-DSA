c278ca2cfac7717751a96b085cf7638633677afc
Hattinagre Rahul Govind <157114025+rahulhattinagre@users.noreply.github.com>
2026-02-22 22:44:08 +0530

    Create bitManupOperation.java

---
+++ bitManupOperation.java
@@ -0,0 +1,37 @@
+public class bitOperation{
+public static int getIthBit(int n,int i){
+        int bitmask=1<<i;
+        if((n & bitmask)==0){
+            return  0;
+        }
+        else{
+            return 1;
+        }
+    }
+    public static int setIthBit(int n,int i){
+        int bitMask=1<<i;
+        return n| bitMask;
+    }
+    public static int clearIthBit(int n,int i){
+        int bitMask=~(1<<i);
+
+        return n & bitMask;
+    }
+  
+    public static void main(String []args){
+        /*System.out.println(5&6);
+        System.out.println(5|6);
+        System.out.println(5^6);
+        System.out.println(~5);
+        System.out.println(5<<2);
+        System.out.println(6>>1);*/
+        //evenOrOdd(5);
+        //evenOrOdd(6);
+        //evenOrOdd(8);
+       // System.out.println(getIthBit(10,2));
+        //System.out.println(setIthBit(10,2));
+        //System.out.println(clearIthBit(10,1));
+        System.out.println(updateIthBit(10,2,1));
+    }
+    
+}
