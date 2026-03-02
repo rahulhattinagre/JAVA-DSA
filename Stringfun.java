cec6d8d3dad26da5786c31e50900464c66e2fb59
Hattinagre Rahul Govind <157114025+rahulhattinagre@users.noreply.github.com>
2026-03-02 18:16:08 +0530

    Create Stringfun.java

---
+++ Stringfun.java
@@ -0,0 +1,11 @@
+public class Stringfun{
+  public static void main(String args[]){
+    String str1="Rahul hattinagre";
+    String str2="Nanded";
+    System.out.println(str1.concat(str2));
+    System.out.println(str1.toUpperCase());
+    System.out.println(str1.toLowerCase());
+    System.out.println(str1.CharAt(2));
+    System.out.println(str1.indexOf('a'));    
+  }
+}
