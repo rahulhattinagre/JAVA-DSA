118a8bdb2ed18d6af69b7ad4f719cfe00f2a81b6
Hattinagre Rahul Govind <157114025+rahulhattinagre@users.noreply.github.com>
2026-03-16 20:28:35 +0530

    Update Stringfun.java

--- Stringfun.java
+++ Stringfun.java
@@ -19,7 +19,7 @@ public class Stringfun{
     System.out.println(str1.contains(str2));
     System.out.println(str1.compareTo(str2));
     System.out.println(str1.matches(str2));
-    System.out.println("java".equalsIgnoreCase("JAVA")); // true
+System.out.pribtln(str1.repeat(str2)); System.out.println("java".equalsIgnoreCase("JAVA")); // true
     
   }
 }
