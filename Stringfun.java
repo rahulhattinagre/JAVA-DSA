c19acf46b4c363491412474f2bf504a546bce26a
Hattinagre Rahul Govind <157114025+rahulhattinagre@users.noreply.github.com>
2026-03-23 21:29:36 +0530

    Update Stringfun.java

--- Stringfun.java
+++ Stringfun.java
@@ -4,21 +4,13 @@ public class Stringfun{
     String str2="Nanded";
     System.out.println(str1.concat(str2));
    System.out.println(str1.contains(str2)); System.out.println(str1.toUpperCase());
-System.out.println(str1.lastIndexOf(str2))
+  System.out.println(str1.lastIndexOf(str2))
 System.out.println(str1.toLowerCase());
 System.out.println(str1.CharAt(2));
 System.out.ptintln(str1.lastIndexOf(str2))   System.out.println(str1.indexOf('a'));  
 System.out.println(str1.endsWith(str2))
  System.out.println(str1.replace('Nanded','Pune'));
-    System.out.println(str1.subString(hul));
-    System.out.println(str1.split( ));
-    System.out.println(str1.equal(str2));
-    System.out.println(str1.startsWith("Rahul"));
-    System.out.println(str1.trim(str2));
-    System.out.println(str1.endsWith(str2));
-    System.out.println(str1.contains(str2));
-    System.out.println(str1.compareTo(str2));
-    System.out.println(str1.matches(str2));
+   
 System.out.pribtln(str1.repeat(str2)); System.out.println("java".equalsIgnoreCase("JAVA")); // true
     
   }
