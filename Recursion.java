89cddc36eba7552199902589e63a9e67ea633b04
Rahul Hattinagre <rahulhattinagre@gmail.com>
2026-04-18 20:09:55 +0530

    Complete project structure: Final deployment fix

--- Recursion.java
+++ Recursion.java
@@ -54,8 +54,36 @@ public class Recursion {
         }
         return isSorted(arr, i+1);
     }
+    public static void accurance(int arr[],int key,int i){
+        if(i==arr.length){
+            return;
+        }
+        if(arr[i]==key){
+            System.out.print(i+" ");
+        }
+        accurance(arr, key, i+1);
+    }
+    //number to string 
+     static String[] digits = {
+        "zero", "one", "two", "three", "four",
+        "five", "six", "seven", "eight", "nine"
+    };
+
+    public static void printNumber(int n) {
+        // Base case
+        if (n == 0) {
+            return;
+        }
+
+        // Recursive call
+        printNumber(n / 10);
+
+        // Print last digit
+        int lastDigit = n % 10;
+        System.out.print(digits[lastDigit] + " ");
+    }
     public static void main(String[] args){
-        int arr[]={1,2,3,6,5};
-       System.out.println(isSorted(arr, 0));
+        //int arr[]={3,2,4,5,6,2,7,2,2};
+        printNumber(1947);
     }
     }
