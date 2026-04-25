5ae15837dfae70c749c8ae2c4f76ad554dc526ed
Rahul Hattinagre <rahulhattinagre@gmail.com>
2026-04-25 12:40:55 +0530

    Complete project structure: Final deployment fix

---
+++ DividenConquer.java
@@ -0,0 +1,41 @@
+public class DividenConquer{
+    public static int search(int arr[],int target,int si,int ei){
+        //basecase
+        if(si>ei){
+            return-1;
+        }
+        //find mid
+        int mid=si+(ei-si)/2;//(si+ei)/2
+
+        //case foundcheck mid is target
+        if(arr[mid]==target){
+            return mid;
+        }
+
+        //mid on L1;
+        if(arr[si]<=arr[mid]){
+            //case a left side
+            if(arr[si]<=target && target<arr[mid]){
+                return search(arr,target,si,mid-1);
+            }
+            //right side
+            else{
+                return search(arr, target, mid+1, ei);
+        }
+    }else{
+            //case right side of L2
+            if(arr[mid]<=target && target<=arr[ei]){
+                return search(arr, target, mid+1, ei);
+            }
+            else{
+                return search(arr, target, si, mid-1);
+            }
+        }
+    }
+    public static void main(String[] args){
+        int arr[]={4,5,6,7,0,1,2};
+        int target=6;
+        int tarindx=search(arr,target,0,arr.length-1);
+        System.out.println(tarindx);
+    }
+}
