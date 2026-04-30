3c09419e0ae6ca04c432c300965b7de8f59b41ec
Rahul Hattinagre <rahulhattinagre@gmail.com>
2026-05-01 00:03:11 +0530

    Complete project structure: Final deployment fix

--- Backtracking.java
+++ Backtracking.java
@@ -16,10 +16,31 @@ public class Backtracking {
         }
         System.out.println();
     }
+
+    //findsubsets of String
+    public static void findSubsets(String str,String ans,int i){
+        //base case
+        if(i== str.length()){
+            if(ans.length()== 0){
+                System.out.println("Null");
+            }else{
+                System.out.println(ans);
+            }
+            return;
+        }
+
+        //recurssion 
+        //YES choice
+        findSubsets(str, ans+str.charAt(i), i+1);
+        //NO choice
+        findSubsets(str, ans, i+1);
+
+    } 
     public static void main(String[] args){
-        int arr[]=new int[5];
-        changearr(arr,0,1);
-        printarr(arr);
+        //int arr[]=new int[5];
+        //changearr(arr,0,1);
+        //printarr(arr);
+        String str="abc";
+        findSubsets(str, " ", 0);
     }
-    
 }
---
+++ NQueen.java
@@ -0,0 +1,72 @@
+public class NQueen {
+
+    public static boolean isSafe(char board[][], int row, int col) {
+
+        // vertical up
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // diagonal left up
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // diagonal right up
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // N-Queen function
+    public static void nQueen(char board[][], int row) {
+
+        // base case
+        if (row == board.length) {
+            printBoard(board);
+            return;
+        }
+
+        // column loop
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                nQueen(board, row + 1); // recursive call
+                board[row][j] = 'X'; // backtracking
+            }
+        }
+    }
+
+    public static void printBoard(char board[][]) {
+        System.out.println("-------Chess Board-------");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        char board[][] = new char[n][n];
+
+        // initialize board
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = 'X';
+            }
+        }
+
+        nQueen(board, 0);
+    }
+}
