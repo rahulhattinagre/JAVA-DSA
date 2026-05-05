d1272b5f48da42730f3263b54f7364ce2d74dfb6
Hattinagre Rahul Govind <157114025+rahulhattinagre@users.noreply.github.com>
2026-05-05 10:43:57 +0530

    Add gridWaysCount method for grid path counting

--- BacktrackingGrid.java
+++ BacktrackingGrid.java
@@ -1,4 +1,20 @@
 public class BacktrackingGrid{
+  public static int gridWaysCount(int i,int j,int n,int m){
+        //base case
+        if(i==n-1&& j==m-1){
+            return 1;
+
+        }else if(i==n || j==m){
+            return 0;
+
+        }
+        //recurssion
+        int w1=gridWaysCount(i+1, j, n, m);
+        int w2=gridWaysCount(i, j+1, n, m);
+        return w1 + w2;
+    }
 public static void main(String []args){
+    int n=3, m=3;
+        System.out.println(gridWaysCount(0, 0, n, m));
 }
 }
