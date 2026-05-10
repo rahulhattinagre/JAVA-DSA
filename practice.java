a0db145aebab4f82f3acca3f90272198d60cc025
Rahul Hattinagre <rahulhattinagre@gmail.com>
2026-05-11 00:02:28 +0530

    Removed class files

--- practice.java
+++ practice.java
@@ -1,30 +1,22 @@
 public class practice {
+    public static int searchKey(int num[],int key){
+        int start=0 ,end=num.length-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(num[mid]==key){
+                return mid;
+            }
+            else if(num[mid]>key){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return -1;
+        }
     public static void main(String[] args) {
-        int n=4;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            for(int k=1;k<=n-i;k++){
-                System.out.print("  ");
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        //bottom half
-        for(int i=n;i>=1;i--){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            for(int k=1;k<=n-i;k++){
-                System.out.print("  ");
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        int num[]= {1,2,3,4,5,6,7,8,9,5};
+        int key=5;
+        System.out.println(searchKey(num, key));
     }
 }
