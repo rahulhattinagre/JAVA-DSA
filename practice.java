6b8ab4851f02e48aaa307597fe775edc1920a729
Hattinagre Rahul Govind <157114025+rahulhattinagre@users.noreply.github.com>
2026-05-22 00:07:43 +0530

    Refactor bubble sort with optimizations and metrics
    Refactored bubble sort implementation to include optimization and tracking of comparisons and swaps.

--- practice.java
+++ practice.java
@@ -1,24 +1,55 @@
-public class practice {
-    public static void bubblesort(int arr[]){
-        for(int turn=0;turn<arr.length-1;turn++){
-            for(int j=0;j<arr.length-1-turn;j++){
-                if(arr[j]>arr[j+1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+public class Practice {
+
+    // Advanced Bubble Sort
+    public static void bubbleSort(int arr[]) {
+        int comparisons = 0;
+        int swaps = 0;
+
+        for (int turn = 0; turn < arr.length - 1; turn++) {
+            boolean swapped = false; // optimization
+
+            for (int j = 0; j < arr.length - 1 - turn; j++) {
+                comparisons++;
+
+                if (arr[j] > arr[j + 1]) {
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swaps++;
+                    swapped = true;
                 }
             }
-        }
-        }
-        public static void printsort(int arr[]){
-            for(int i=0;i<arr.length;i++){
-                System.out.print(arr[i]+" ");
-            }
 
+            // If no swapping, array is already sorted
+            if (!swapped) {
+                System.out.println("Array already sorted. Stopping early.");
+                break;
+            }
         }
+
+        System.out.println("Total Comparisons = " + comparisons);
+        System.out.println("Total Swaps = " + swaps);
+    }
+
+    // Print array
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        int arr[]= {6,5,6,7,8,2,};
-        bubblesort(arr);
-        printsort(arr);
+        int arr[] = {6, 5, 6, 7, 8, 2};
+
+        System.out.println("Original Array:");
+        printArray(arr);
+
+        bubbleSort(arr);
+
+        System.out.println("Sorted Array:");
+        printArray(arr);
     }
 }
