ddc255aaae019ab1f0676e1f4c15bd4584e4ea16
Hattinagre Rahul Govind <157114025+rahulhattinagre@users.noreply.github.com>
2026-05-26 23:53:30 +0530

    Update test cases in main method
    Commented out previous test cases and added a new array for testing.

--- DividenConquer.java
+++ DividenConquer.java
@@ -50,7 +50,7 @@ public class DividenConquer{
         return -1;
     }
     public static void main(String[] args){
-        //int arr[]={4,5,6,7,0,1,2};
+        //int arr[]={2,3,4,5,6,7,8,9};
         //int target=6;
         //int tarindx=search(arr,target,0,arr.length-1);
         //System.out.println(tarindx);
