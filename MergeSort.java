d3261f621ed711b60c3c16283f75283a5a4e2dac
Hattinagre Rahul Govind <157114025+rahulhattinagre@users.noreply.github.com>
2026-05-28 22:54:58 +0530

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -1,93 +1,113 @@
-public class MergeSort {
-    
-    public static void printarr(String arr[]){
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
+import java.util.Arrays;
+
+public class MergeSortAdvanced {
+
+    // Print Array
+    public static <T> void printArr(T arr[]) {
+        for (T element : arr) {
+            System.out.print(element + " ");
         }
         System.out.println();
     }
 
-    public static void mergeSort(int arr[], int si, int ei){
-        if(si >= ei){
+    // Generic Merge Sort
+    public static <T extends Comparable<T>> void mergeSort(T arr[], int si, int ei, boolean ascending) {
+
+        if (si >= ei) {
             return;
         }
-        int mid = si + (ei - si)/2;
 
-        mergeSort(arr, si, mid);       // left
-        mergeSort(arr, mid+1, ei);     // right
+        int mid = si + (ei - si) / 2;
 
-        merge(arr, si, mid, ei);
+        // Left Part
+        mergeSort(arr, si, mid, ascending);
+
+        // Right Part
+        mergeSort(arr, mid + 1, ei, ascending);
+
+        // Merge
+        merge(arr, si, mid, ei, ascending);
     }
 
-    public static void merge(int arr[], int si, int mid, int ei){
-        int temp[] = new int[ei - si + 1];
+    // Generic Merge Function
+    public static <T extends Comparable<T>> void merge(T arr[],
+                                                        int si,
+                                                        int mid,
+                                                        int ei,
+                                                        boolean ascending) {
+
+        Object temp[] = new Object[ei - si + 1];
 
         int i = si;
         int j = mid + 1;
         int k = 0;
 
-        while(i <= mid && j <= ei){
-            if(arr[i] <= arr[j]){
+        while (i <= mid && j <= ei) {
+
+            int compare = arr[i].compareTo(arr[j]);
+
+            if (ascending ? compare <= 0 : compare >= 0) {
                 temp[k++] = arr[i++];
             } else {
                 temp[k++] = arr[j++];
             }
         }
 
-        while(i <= mid){
+        // Left Part
+        while (i <= mid) {
             temp[k++] = arr[i++];
         }
 
-        while(j <= ei){
+        // Right Part
+        while (j <= ei) {
             temp[k++] = arr[j++];
         }
 
-        for(k = 0, i = si; k < temp.length; k++, i++){
-            arr[i] = temp[k];
+        // Copy temp to original array
+        for (k = 0, i = si; k < temp.length; k++, i++) {
+            arr[i] = (T) temp[k];
         }
     }
-    //applying Merge Sort on an array of Strings:
-    public static void mergeString(String arr[],int si,int ei){
-        if(si>=ei){
-            return;
-        }
-        int mid=si+(ei-si)/2;
-        mergeString(arr, si, mid);//left
-        mergeString(arr, mid+1, ei);//right
-        merge2(arr, si, mid, ei);
-    }
-    public static void merge2(String arr[],int si,int mid,int ei)
-    {
-        String temp[]=new String[ei-si+1];
-        int i=si;
-        int j=mid+1;
-        int k=0;
 
-        while(i<=mid && j<=ei){
-            if (arr[i].compareTo(arr[j]) <= 0){
-                temp[k++]=arr[i++];
-            }
-            else{
-                temp[k++]=arr[j++];
-            }
-        }
-        //left part
-        while(i<=mid){
-            temp[k++]=arr[i++];
-        }
-        //right part
-        while(j<=ei){
-            temp[k++]=arr[j++];
-        }
-    for(k=0,i=si;k<temp.length;k++,i++){
-        arr[i]=temp[k];
+    // Case-Insensitive String Sort
+    public static void sortStringsIgnoreCase(String arr[]) {
+        Arrays.sort(arr, String.CASE_INSENSITIVE_ORDER);
     }
-}
+
     public static void main(String[] args) {
-        String arr[] = { "sun", "earth", "mars", "mercury" };
-        mergeString(arr, 0, arr.length - 1);
-        printarr(arr);
-        //mergeSort(arr, 0, arr.length - 1);
-        //printarr(arr);
+
+        // Integer Array
+        Integer nums[] = { 8, 3, 1, 6, 2, 9, 5 };
+
+        long start = System.nanoTime();
+
+        mergeSort(nums, 0, nums.length - 1, true);
+
+        long end = System.nanoTime();
+
+        System.out.println("Ascending Integer Sort:");
+        printArr(nums);
+
+        System.out.println("Execution Time: " + (end - start) + " ns");
+
+        // Descending Sort
+        mergeSort(nums, 0, nums.length - 1, false);
+
+        System.out.println("\nDescending Integer Sort:");
+        printArr(nums);
+
+        // String Array
+        String planets[] = { "sun", "Earth", "mars", "Mercury" };
+
+        mergeSort(planets, 0, planets.length - 1, true);
+
+        System.out.println("\nString Sort:");
+        printArr(planets);
+
+        // Case Insensitive Sort
+        sortStringsIgnoreCase(planets);
+
+        System.out.println("\nCase-Insensitive Sort:");
+        printArr(planets);
     }
 }
