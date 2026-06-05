5c9b58a2979d40eae78abd51990cbc16044264d3
Hattinagre Rahul Govind <157114025+rahulhattinagre@users.noreply.github.com>
2026-06-05 23:37:05 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,12 +1,40 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-      int sum=m+n;
-      for(int i=m-1;i>=n;i++);
+
+        int i = m - 1;      // last valid element in nums1
+        int j = n - 1;      // last element in nums2
+        int k = m + n - 1;  // last position in nums1
+
+        while(i >= 0 && j >= 0) {
+            if(nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        while(j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
     }
-    public static void main(String agrs[]){
-        int num1[]={1,2,3,};
-        int num2[]={2,5,6};
-        int n=3;
-        int m=3;
+
+    public static void main(String[] args) {
+        int nums1[] = {1,2,3,0,0,0};
+        int nums2[] = {2,5,6};
+
+        int m = 3;
+        int n = 3;
+
+        Solution obj = new Solution();
+        obj.merge(nums1, m, nums2, n);
+
+        for(int x : nums1) {
+            System.out.print(x + " ");
+        }
     }
 }
