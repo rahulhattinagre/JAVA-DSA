4644512ffdd009f56bc0368e9c9139e9baa2ed3a
Hattinagre Rahul Govind <157114025+rahulhattinagre@users.noreply.github.com>
2026-06-09 19:40:04 +0530

    Refactor Solution class to implement swapping
    Removed the merge function and added a swap implementation in the main method.

--- Solution.java
+++ Solution.java
@@ -1,43 +1,16 @@
-class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-
-        int i = m - 1;      // last valid element in nums1
-        int j = n - 1;      // last element in nums2
-        int k = m + n - 1;  // last position in nums1
-
-        while(i >= 0 && j >= 0) {
-            if(nums1[i] > nums2[j]) {
-                nums1[k] = nums1[i];
-                i--;
-            } else {
-                nums1[k] = nums2[j];
-                j--;
-            }
-            k--;
-        }
-
-        while(j >= 0) {
-            nums1[k] = nums2[j];
-            j--;
-            k--;
-        }
-    }
-
+public class Swap {
     public static void main(String[] args) {
-        int nums1[] = {1,2,3,0,0,0};
-        int nums2[] = {2,5,6};
+        int a = 10;
+        int b = 20;
 
-        int m = 3;
-        int n = 3;
+        System.out.println("Before Swapping:");
+        System.out.println("a = " + a + ", b = " + b);
 
-        Solution obj = new Solution();
-        obj.merge(nums1, m, nums2, n);
+        a = a + b;
+        b = a - b;
+        a = a - b;
 
-         Solution obj1= new Solution();
-        obj.merge(nums1, m, nums2, n);
-
-        for(int x : nums1) {
-            System.out.print(x + " ");
-        }
+        System.out.println("After Swapping:");
+        System.out.println("a = " + a + ", b = " + b);
     }
 }
