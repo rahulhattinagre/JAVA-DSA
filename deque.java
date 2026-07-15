e52e7ef122014eb00f46cf1d4530b69d3c8a2f5d
Hattinagre Rahul Govind <157114025+rahulhattinagre@users.noreply.github.com>
2026-07-15 23:05:02 +0530

    Update deque.java

--- deque.java
+++ deque.java
@@ -1,1 +1,21 @@
 
+import java.util.*;
+public class Deque {
+
+    public static void main(String args[]) {
+        Deque<Integer> q = new LinkedList<>();
+        q.addFirst(4);
+        q.addFirst(3);
+        q.addFirst(2);
+        q.addFirst(1);
+
+        q.addLast(5);
+        q.addLast(6);
+        q.removeFirst();
+
+        while (!q.isEmpty()) {
+            System.out.print(q.remove());
+        }
+        System.out.println();
+    }
+}
