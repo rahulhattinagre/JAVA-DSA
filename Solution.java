dbc3f4d0e9bc8c7d954d35e58ec02bfb27e12b90
Hattinagre Rahul Govind <157114025+rahulhattinagre@users.noreply.github.com>
2026-07-18 23:58:22 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,12 +1,57 @@
-class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-      int sum=m+n;
-      for(int i=m-1;i>=n;i++);
+class address {
+    String city;
+
+    address(String city) {
+        this.city = city;
     }
-    public static void main(String agrs[]){
-        int num1[]={1,2,3,0,0,0};
-        int num2[]={2,5,6};
-        int n=3;
-        int m=3;
+}
+
+class Student {
+    String name;
+    int age;
+    address addr;
+
+    // parameterized constructor
+    Student(String name, int age, address addr) {
+        this.name = name;
+        this.age = age;
+        this.addr = addr;
+    }
+
+    // 🔹 Shallow Copy Constructor
+    Student(Student s, boolean shallow) {
+        this.name = s.name;
+        this.age = s.age;
+        this.addr = s.addr;   //same reference
+    }
+
+    // 🔹 Deep Copy Constructor
+    Student(Student s) {
+        this.name = s.name;
+        this.age = s.age;
+        this.addr = new address(s.addr.city); //  new object
+    }
+}
+
+public class ShallowDeep_copy {
+    public static void main(String[] args) {
+
+        address a1 = new address("Pune");
+        System.out.println(a1.city);
+        Student s1 = new Student("Rahul", 22, a1);
+
+        // 🔹 Shallow Copy
+        Student s2 = new Student(s1, true);
+
+        // 🔹 Deep Copy
+        Student s3 = new Student(s1);
+
+        s2.addr.city = "Mumbai";
+        System.out.println("Shallow Copy:");
+        System.out.println(s1.addr.city); // changes
+
+        s3.addr.city = "Delhi";
+        System.out.println("Deep Copy:");
+        System.out.println(s1.addr.city); //  unchanged
     }
 }
