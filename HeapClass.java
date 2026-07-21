f7716c09ff9397b898179bb7f94f1d555cafbc00
Rahul Hattinagre <rahulhattinagre@gmail.com>
2026-07-21 12:17:28 +0530

    heap added removed and sort

--- HeapClass.java
+++ HeapClass.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 
 public class HeapClass {
@@ -10,14 +11,12 @@ public class HeapClass {
             //add at last automatically arraylist
             arr.add(data);
             int x = arr.size() - 1;//last index;
-
             while (x > 0) {
                 int par = (x - 1) / 2;//get parent
                 if (arr.get(x) < arr.get(par)) {
                     int temp = arr.get(x);
                     arr.set(x, arr.get(par));
                     arr.set(par, temp);
-
                     x = par;
                 } else {
                     break;
@@ -25,6 +24,52 @@ public class HeapClass {
             }
         }
 
+        public int peek() {
+            return arr.get(0);
+        }
+
+        //heapify
+        public void heapify(int i) {
+            int mini = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < arr.size() && arr.get(left) < arr.get(mini)) {
+                mini = left;
+            }
+            if (right < arr.size() && arr.get(mini) > arr.get(right)) {
+                mini = right;
+            }
+
+            if (mini != i) {
+                int temp = arr.get(i);
+                arr.set(i, arr.get(mini));
+                arr.set(mini, temp);
+
+                heapify(mini);
+            }
+        }
+
+        public int remove() {
+            //get 0 index
+            int data = arr.get(0);
+
+            //swap both
+            int temp = arr.get(0);
+            arr.set(0, arr.get(arr.size() - 1));
+            arr.set(arr.size() - 1, temp);
+
+            //after swaping remove leaf
+            arr.remove(arr.size() - 1);
+
+            //call heap beacause fix the heap
+            heapify(0);
+            return data;
+        }
+
+        public boolean isEmpty() {
+            return arr.size() == 0;
+        }
     }
 
     public static void main(String[] args) {
@@ -34,7 +79,11 @@ public class HeapClass {
         h.add(4);
         h.add(5);
         h.add(10);
-        h.add(1);
-        System.out.print(h.arr);
+
+        while (!h.isEmpty()) {
+            System.out.print(h.peek() + "  ");
+            h.remove();
+        }
+
     }
 }
