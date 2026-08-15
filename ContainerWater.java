c76ac5ca78a631b774381adca73b9117ca1bc8b0
Hattinagre Rahul Govind <157114025+rahulhattinagre@users.noreply.github.com>
2026-08-15 17:20:53 +0530

    Refactor water storage calculation to use two-pointer approach

--- ContainerWater.java
+++ ContainerWater.java
@@ -1,29 +1,23 @@
-import java.util.ArrayList;
-public class ContainerWater {
-    public static int storeWater(ArrayList<Integer> hight){
-        int maxWater=0;
-        for(int i=0;i<hight.size();i++){
-            for(int j=i+1;j<hight.size();j++){
-                int ht=Math.min(hight.get(i),hight.get(j));
-                int width=j-i;
-                int currentWater=ht*width;
-                maxWater=Math.max(maxWater,currentWater);
-            }
+class ContainerWater {
+    public int maxArea(int[] height) {
+      int n=height.length;
+      int left=0;
+      int right=n-1;
+      int maxwater=0;
+      while(left<right){
+        int width=right-left;
+        int h=Math.min(height[left],height[right]);
+        int area=width*h;
+
+        maxwater=Math.max(maxwater,area);
+        if(height[left]<height[right]){
+            left++;
+
+        }else{
+            right--;
         }
-        return maxWater;
-    }
-   
-    public static void  main(String []args){
-        ArrayList<Integer> hight=new ArrayList<>();
-        hight.add(1);
-        hight.add(8);
-        hight.add(6);
-        hight.add(2);
-        hight.add(5);
-        hight.add(4);
-        hight.add(7);
-        hight.add(3);
-        hight.add(7);
-        System.out.println(storeWater(hight));
+      }
+      return maxwater;
+
     }
 }
